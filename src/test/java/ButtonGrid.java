48003b0432cc2b46a13ebfcd2d4e19ceca06cf09
Eugene Ivanov <evgenmoskva001@gmail.com>
2018-03-26 23:44:44 +0500

    - ADD: new methods and code refactoring

--- src/test/java/ButtonGrid.java
+++ src/test/java/ButtonGrid.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 
 /**
  * @author Eugene Ivanov on 26.03.2018
@@ -15,73 +14,66 @@ public class ButtonGrid {
     private JFrame frame;
     private JLabel[][] grid;
     private Border border;
-    private ImageIcon targetImage;
     private Map<Integer, JLabel> cellMap;
-    private Function<Integer, Integer> keyMapFunction;
 
-    public void createGui(int width, int height, int cellX, int cellY){ // constructor of class
+    public void createGui(int width, int height){ // constructor of class
         frame = new JFrame("Simple Animation"); // create a frame
         frame.setLayout(new GridLayout(width,height)); // set layout of frame
-
         grid = new JLabel[width][height]; // create a grid from many cells (size in choose)
-
         border = BorderFactory.createLineBorder(Color.BLACK, 1);
 
-        URL appIconURL = getClass().getResource("/bananas.png");
-        URL targetImageURL = getClass().getResource("/cherries.png");
-
-        // checking for existing image files
-        if (appIconURL == null) {
-            System.out.println("Could not find image!");
-        } else {
-            frame.setIconImage(new ImageIcon(appIconURL).getImage());
-        }
-
-        if (targetImageURL == null) {
-            System.out.println("Could not find image!");
-        } else {
-            targetImage = new ImageIcon(getClass().getResource("/cherries.png"));
-        }
-
-        cellX--;
-        cellY--;
+        frame.setIconImage(new ImageIcon(
+                checkingUrl("/bananas.png")).getImage());
 
         cellMap = new HashMap<>();
-        Integer key;
+        int key;
 
         for (int y = height - 1; y >= 0; y--) {
             for (int x = 0; x < width; x++) {
                 // create a new button/other gui-element
                 grid[x][y] = new JLabel("(" + (x + 1) + "," + (y + 1) + ")");
                 grid[x][y].setBorder(border);
-
-                key = Integer.parseInt(String.valueOf(x + 1) + String.valueOf(y + 1));
+                key = getCoordinates(x + 1, y + 1);
                 cellMap.put(key, grid[x][y]);
-
-                System.out.println("key = " + key);
-
-                if (x == cellX && y == cellY) {
-                    grid[cellX][cellY].setBackground(Color.YELLOW);
-                    grid[cellX][cellY].setIcon(targetImage);
-                    grid[cellX][cellY].setOpaque(true);
-                }
                 frame.add(grid[x][y]); // add element at grid
             }
         }
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // default close operation
         frame.pack(); // set appropriate frame size
         frame.setLocation(500, 100); // location of fram at user screen
-        frame.setSize(500, 500); // standard size of frame
+        frame.setSize(700, 700); // standard size of frame
         frame.setVisible(true); // set option "visible" of frame
+    }
+
+    private static int getCoordinates(int x, int y) {
+        return Integer.parseInt(String.valueOf(x) + String.valueOf(y));
+    }
+
+    private static URL checkingUrl(String resourceName) {
+        URL resourceUrl = ButtonGrid.class.getResource(resourceName);
+        if (resourceUrl == null) {
+            System.out.println("Could not find resource!");
+            resourceUrl = ButtonGrid.class.getResource("/");
+        }
+        return resourceUrl;
     }
 
     public static void main(String[] args) {
         ButtonGrid buttonGrid = new ButtonGrid();
-        buttonGrid.createGui(   5,5, 1, 2);
 
-        buttonGrid.cellMap.get(55).setBackground(Color.GREEN);
-        buttonGrid.cellMap.get(55).setOpaque(true);
+        buttonGrid.createGui(   10,10);
 
-        //buttonGrid.frame.repaint();
+        // coordinates of cherries
+        int cherriCoordinates = getCoordinates(5, 3);
+        buttonGrid.cellMap.get(cherriCoordinates).setBackground(Color.YELLOW);
+        buttonGrid.cellMap.get(cherriCoordinates).setOpaque(true);
+        buttonGrid.cellMap.get(cherriCoordinates).setIcon(
+                new ImageIcon(checkingUrl("/cherries.png"))
+        );
+
+        // coordinates of color cell
+        int targetCoordinates = getCoordinates(5, 4);
+        buttonGrid.cellMap.get(targetCoordinates).setBackground(Color.GREEN);
+        buttonGrid.cellMap.get(targetCoordinates).setOpaque(true);
     }
 }
