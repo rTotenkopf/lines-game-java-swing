b8a634aa26cfaa02a5e9828df285e64a30a868e1
Eugene Ivanov <evgenmoskva001@gmail.com>
2018-04-08 22:10:47 +0500

    - ADD: new variants of mainframe size in class RunLines.java

--- src/main/java/com/game/lines/RunLines.java
+++ src/main/java/com/game/lines/RunLines.java
@@ -27,6 +27,22 @@ public class RunLines {
     }
 
     public static void main(String[] args) {
-        new MainFrame(540,570, 9, 9);
+        // 7 X 7
+//        new MainFrame(   395,470, 7, 7);
+
+        // 8 X 8
+//        new MainFrame(   480,555, 8, 8);
+
+        // 9 X 9
+        new MainFrame(   565,640, 9, 9);
+
+        // 10 X 10
+//        new MainFrame(   600,630, 10, 10);
+
+        // 11 X 11
+//        new MainFrame(   660,690, 11, 11);
+
+        // 12 X 12
+//        new MainFrame(   690,770, 12, 12);
     }
 }
