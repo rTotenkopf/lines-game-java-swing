2b6b03cf144afc8e54979db2f01b120f5bd5a001
Eugene Ivanov <evgenmoskva001@gmail.com>
2018-04-22 23:26:13 +0500

    - FIX: State.java file was moved into "logic" package.

---
+++ src/main/java/com/game/lines/logic/State.java
@@ -0,0 +1,11 @@
+package com.game.lines.logic;
+
+/**
+ * @author Eugene Ivanov on 13.04.18
+ */
+
+public enum State {
+    SELECTED,   // Ячейка выбрана.
+    RELEASED,   // Ячейка освобождена.
+    EMPTY       // Ячейка пуста.
+}
