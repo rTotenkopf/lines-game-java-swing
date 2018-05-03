28a59175a79f32f8c89d8e8d709fddb9c896d20b
Eugene Ivanov <evgenmoskva001@gmail.com>
2018-05-03 21:52:48 +0500

    - ADD: Comments to Clickable.java interface.

--- src/main/java/com/game/lines/logic/Clickable.java
+++ src/main/java/com/game/lines/logic/Clickable.java
@@ -3,10 +3,14 @@ package com.game.lines.logic;
 import java.awt.event.MouseListener;
 
 /**
+ * Интерфейс Clickable наследует MouseListener (слушатель мыши) и делает объекты класса, реализующего интерфейс,
+ * "кликабельными", т.е. с объектами можно будет выполнять действия с помощью кликов мыши.
+ * Кроме наследуемых методов, интерфейс предоставляет 2 своих собственных метода.
+ *
  * @author Eugene Ivanov on 11.04.18
  */
 
 public interface Clickable extends MouseListener {
-    void select();
-    void release();
+    void select();  // Выбрать (выделить) объект.
+    void release(); // Освободить (снять выделение) объект.
 }
