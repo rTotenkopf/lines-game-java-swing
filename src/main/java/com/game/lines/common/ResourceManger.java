30c1dd098c2197e16dde6122cf96acd99f07e8c3
Eugene Ivanov <evgenmoskva001@gmail.com>
2018-05-25 22:31:08 +0500

    - FIX: code of ResourceManager.java class refactored.

--- src/main/java/com/game/lines/common/ResourceManger.java
+++ src/main/java/com/game/lines/common/ResourceManger.java
@@ -11,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 /**
@@ -21,60 +22,54 @@ import java.util.stream.Stream;
 
 public class ResourceManger {
 
-    // TODO: Обычно константы задаются большими буквами
-    // TODO: А инициализацию делать лучше всё таки в конструкторе
+    // Логгер процесса работы с ресурсами.
+    private static Logger resourceLogger = Logger.getLogger(ResourceManger.class.getName());
     // URL иконки окна игры.
-    private static final URL imageIconUrl = ResourceManger.class.getResource("/food/bananas.png");
-
+    private static final URL IMAGE_ICON_URL ;
+    // Инициализация константы.
+    static {
+        IMAGE_ICON_URL = ResourceManger.class.getResource("/food/bananas.png");
+    }
     // Получаем изображение иконки окна игры, используя URL.
     public static Image getImage() {
-        return imageIconUrl != null ? new ImageIcon(imageIconUrl).getImage() : null;
+        return IMAGE_ICON_URL != null ? new ImageIcon(IMAGE_ICON_URL).getImage() : null;
     }
     // Массив изображений используемых в игре.
     public static final Object[] BALLS = ballsMap().values().toArray();
 
     /**
-     * Key = название цвета; Value = изображение;
+     * Key = название цвета;
+     * Value = изображение;
      * @return карта изображений по цветам.
      */
     public static Map<String, ImageIcon> ballsMap() {
         Map<String, ImageIcon> iconMap = new HashMap<>();
         // Ссылка на ресурс (папку с изображениями).
-
         URL resourceUrl = RunLines.class.getResource("/balls/");
         // Обход файлов, находящихся в папке ресурса.
-
         if (resourceUrl != null) {
-            // TODO весь блок try/catch рекомендуется выноосить в отдельный метод, так и читабельнее будет
-            try (Stream<Path> paths = Files.walk(Paths.get(resourceUrl.toURI()))) {
-                paths
-                        .filter(Files::isRegularFile)
-                        .forEach(file -> {
-                            // TODO: Так же в отдельный метод выносят логику отдельного элемента в цикле
-                            // TODO: но тут в принципе 4 строчки, поэтому норм
-                            // TODO: System.out.println в качестве тестирования пойдёт, но лучше сразу закладывать использование логгера
-                            // TODO: Когда значение, которые ты хочешь передать строится как-то сложно, то лучше задать говорящую переменную:
-                            // TODO: *******************************************************************************
-                            // TODO: String color = file.getFileName().toString().split("[.-]")[0];
-                            // TODO: String image = "/" + file.getParent().getFileName() + "/" + file.getFileName();
-                            // TODO: iconMap.put(color, new ImageIcon(RunLines.class.getResource(image)));
-                            // TODO: *******************************************************************************
-
-                            URL url = RunLines.class.getResource(
-                                    "/" + file.getParent().getFileName() + "/" + file.getFileName());
-
-                            String[] fileName = file.getFileName().toString().split("[.-]");
-//                            System.out.println(fileName[0]);
-                            ImageIcon imageIcon = new ImageIcon(url);
-                            iconMap.put(fileName[0], imageIcon);
-//                            System.out.println(url.getFile());
-                        });
-            } catch (URISyntaxException | IOException e) {
-                // TODO: плохой тон стектрейс выводить. нужно пользоваться логгером
-                // LOG.warn("Message", e);
-                e.printStackTrace();
-            }
+            iconMap = getImagesFromResource( resourceUrl );
         }
         return iconMap;
+    }
+
+    private static Map<String, ImageIcon> getImagesFromResource(URL resourceUrl) {
+        Map<String, ImageIcon> map = new HashMap<>();
+        try (Stream<Path> paths = Files.walk(Paths.get(resourceUrl.toURI()))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(file -> {
+                        // Получаем название цвета изображения, содержащееся в имени файла.
+                        String color = file.getFileName().toString().split("[.-]")[0];
+//                            resourceLogger.info(color);
+//                            resourceLogger.info(url.getFile());
+                        String image = "/" + file.getParent().getFileName() + "/" + file.getFileName();
+                        // Добавляем ImageIcon в карту, где Key = цвет, Value = ImageIcon.
+                        map.put(color, new ImageIcon( RunLines.class.getResource(image) ));
+                    });
+        } catch (URISyntaxException | IOException e) {
+            resourceLogger.warning(e.getMessage());
+        }
+        return map;
     }
 }
