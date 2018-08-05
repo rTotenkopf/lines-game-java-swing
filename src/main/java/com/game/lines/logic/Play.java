bceabb50805e6a037ee210051f5eff47dfe9a22c
Eugene Ivanov <evgenmoskva001@gmail.com>
2018-08-05 17:18:13 +0500

    - FIX: logger message typo in Play class.

--- src/main/java/com/game/lines/logic/Play.java
+++ src/main/java/com/game/lines/logic/Play.java
@@ -306,7 +306,7 @@ public class Play {
      * @param line коллекция, содержащая ячейки, изображения из которых необходимо удалить.
      */
     private void deleteImagesFromCells(Collection<Cell> line) {
-        playLogger.info("Line of " + line.size() + " images.balls deleted!");
+        playLogger.info("Line of " + line.size() + " images was deleted!");
         gameInfo.setText("Линия из " + line.size() + " шаров удалена!");
         setLineState(true); // Значение true означает, что срока удалена.
         line.forEach( cell -> { // Последовательное удаление изображений из ячеек.
