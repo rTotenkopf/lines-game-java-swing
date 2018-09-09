ad1a27b85a0262f1084e58f2fcd913aa2812a200
Eugene Ivanov <evgenmoskva001@gmail.com>
2018-09-09 20:08:35 +0500

    size of game-field was changed.

--- src/main/java/com/game/lines/Application.java
+++ src/main/java/com/game/lines/Application.java
@@ -12,16 +12,16 @@ public class Application {
 
     public static void main(String[] args) {
         // 7 X 7
-        new MainPanel(   395,470, 7, 7);
+//        new MainPanel(   420,500, 7, 7);
 
         // 8 X 8
-//        new MainPanel(   480,555, 8, 8);
+//        new MainPanel(   466,555, 8, 8);
 
         // 9 X 9
-//        new MainPanel(   565, 640, 9, 9);
+//        new MainPanel(   545, 640, 9, 9);
 
         // 10 X 10
-//        new MainPanel(   600,630, 10, 10);
+        new MainPanel(   600,630, 10, 10);
 
         // 11 X 11
 //        new MainPanel(   660,690, 11, 11);
